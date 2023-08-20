7ed41eba87c47a6bcf3127d1a572caa847f14e95
ZATRANO <36309658+zatrano@users.noreply.github.com>
2023-08-20 21:04:44 +0300

    Update user_service.go

--- internal/app/services/user_service.go
+++ internal/app/services/user_service.go
@@ -21,12 +21,12 @@ func (s *UserService) GetUserByID(id uint) (models.User, error) {
 	return s.repo.GetByID(id)
 }
 
-func (s *UserService) CreateUser(book models.User) (models.User, error) {
-	return s.repo.Create(book)
+func (s *UserService) CreateUser(user models.User) (models.User, error) {
+	return s.repo.Create(user)
 }
 
-func (s *UserService) UpdateUser(book models.User) (models.User, error) {
-	return s.repo.Update(book)
+func (s *UserService) UpdateUser(user models.User) (models.User, error) {
+	return s.repo.Update(user)
 }
 
 func (s *UserService) DeleteUser(id uint) error {
