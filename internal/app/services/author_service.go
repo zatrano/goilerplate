7097379dd39e6cb8571c6ddb90be7604cb136bb3
ZATRANO <36309658+zatrano@users.noreply.github.com>
2023-08-20 21:02:59 +0300

    Update author_service.go

--- internal/app/services/author_service.go
+++ internal/app/services/author_service.go
@@ -21,12 +21,12 @@ func (s *AuthorService) GetAuthorByID(id uint) (models.Author, error) {
 	return s.repo.GetByID(id)
 }
 
-func (s *AuthorService) CreateAuthor(book models.Author) (models.Author, error) {
-	return s.repo.Create(book)
+func (s *AuthorService) CreateAuthor(author models.Author) (models.Author, error) {
+	return s.repo.Create(author)
 }
 
-func (s *AuthorService) UpdateAuthor(book models.Author) (models.Author, error) {
-	return s.repo.Update(book)
+func (s *AuthorService) UpdateAuthor(author models.Author) (models.Author, error) {
+	return s.repo.Update(author)
 }
 
 func (s *AuthorService) DeleteAuthor(id uint) error {
