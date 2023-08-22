2501165f3c47737a1027dcd50ebab810555ba74f
ZATRANO <36309658+zatrano@users.noreply.github.com>
2023-08-22 13:47:29 +0300

    Update book_dto.go

--- internal/app/dtos/book_dto.go
+++ internal/app/dtos/book_dto.go
@@ -2,16 +2,16 @@ package dtos
 
 type BookDTO struct {
 	ID     uint      `json:"id"`
-	Title  string    `json:"title"`
-	Author AuthorDTO `json:"author"`
+	Title  string    `json:"title" validate:"required"`
+	Author AuthorDTO `json:"author" validate:"required"`
 }
 
 type CreateBookDTO struct {
-	Title  string    `json:"title"`
-	Author AuthorDTO `json:"author"`
+	Title  string    `json:"title" validate:"required"`
+	Author AuthorDTO `json:"author" validate:"required"`
 }
 
 type UpdateBookDTO struct {
-	Title  string    `json:"title"`
-	Author AuthorDTO `json:"author"`
+	Title  string    `json:"title" validate:"required"`
+	Author AuthorDTO `json:"author" validate:"required"`
 }
