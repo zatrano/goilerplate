9766e439357d2df89a8a9e3a3d24a7eee57a016b
ZATRANO <36309658+zatrano@users.noreply.github.com>
2023-08-22 13:47:14 +0300

    Update author_dto.go

--- internal/app/dtos/author_dto.go
+++ internal/app/dtos/author_dto.go
@@ -2,13 +2,13 @@ package dtos
 
 type AuthorDTO struct {
 	ID   uint   `json:"id"`
-	Name string `json:"name"`
+	Name string `json:"name" validate:"required"`
 }
 
 type CreateAuthorDTO struct {
-	Name string `json:"name"`
+	Name string `json:"name" validate:"required"`
 }
 
 type UpdateAuthorDTO struct {
-	Name string `json:"name"`
+	Name string `json:"name" validate:"required"`
 }
